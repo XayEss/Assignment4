156792aed2bd72ad565ed87b448740bfe10be6d4
VaradhKaushik <69544057+VaradhKaushik@users.noreply.github.com>
2023-03-15 01:29:13 -0400

    ImageProcessor getChannel implemented

--- src/model/implementation/ImageProcessorImpl.java
+++ src/model/implementation/ImageProcessorImpl.java
@@ -11,7 +11,7 @@ public class ImageProcessorImpl implements ImageProcessor {
 
   @Override
   public Image getChannel(Image image, int channel) {
-    return null;
+    return image.getImageChannel(channel);
   }
 
   @Override
@@ -52,21 +52,21 @@ public class ImageProcessorImpl implements ImageProcessor {
   @Override
   public Image combineGreyScaleImages(Image first, Image second, Image third) {
     Pixel[][] pixels = new PixelRGB[first.getHeight()][first.getWidth()];
-    for(int i = 0; i < first.getHeight(); i++){
-      for (int j = 0; j < first.getWidth(); j++){
-        pixels[i][j] = new PixelRGB(3*first.getPixelChannel(i,j, 0),
-            3*second.getPixelChannel(i,j, 1), 3*third.getPixelChannel(i,j, 2));
+    for (int i = 0; i < first.getHeight(); i++) {
+      for (int j = 0; j < first.getWidth(); j++) {
+        pixels[i][j] = new PixelRGB(3 * first.getPixelChannel(i, j, 0),
+                3 * second.getPixelChannel(i, j, 1), 3 * third.getPixelChannel(i, j, 2));
       }
     }
     return new ImageImpl(pixels);
   }
 
-  private Image combineThreeImages(Image first, Image second, Image third){
+  private Image combineThreeImages(Image first, Image second, Image third) {
     Pixel[][] pixels = new PixelRGB[first.getHeight()][first.getWidth()];
-    for(int i = 0; i < first.getHeight(); i++){
-      for (int j = 0; j < first.getWidth(); j++){
-        pixels[i][j] = new PixelRGB(first.getPixelChannel(i,j, 0),
-            second.getPixelChannel(i,j, 1), third.getPixelChannel(i,j, 2));
+    for (int i = 0; i < first.getHeight(); i++) {
+      for (int j = 0; j < first.getWidth(); j++) {
+        pixels[i][j] = new PixelRGB(first.getPixelChannel(i, j, 0),
+                second.getPixelChannel(i, j, 1), third.getPixelChannel(i, j, 2));
       }
     }
     return new ImageImpl(pixels);
--- test/ImageProcessorTest.java
+++ test/ImageProcessorTest.java
@@ -1,5 +1,23 @@
+import org.junit.Test;
+
+import controller.implementation.ImageUtil;
+import model.implementation.ImageProcessorImpl;
+
+import model.interfaces.Image;
+import model.interfaces.ImageProcessor;
+import model.interfaces.Pixel;
+
 import static org.junit.Assert.*;
 
 public class ImageProcessorTest {
+
+  @Test
+  public void testGetSplitChannels() {
+    Image testImg = new ImageUtil().readFile("resources/images/ppm_testing/testBaseImage.ppm");
+
+    ImageProcessor testProcessor = new ImageProcessorImpl();
+
+
+  }
 
 }
